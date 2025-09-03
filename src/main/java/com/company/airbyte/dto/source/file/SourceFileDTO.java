c385e490b8752916cf6cb611414be481dd67f842
NhanhAnhXtanh <nhatanh08112004@gmail.com>
2025-09-03 17:24:49 +0700

    file

--- src/main/java/com/company/airbyte/dto/source/file/SourceFileDTO.java
+++ src/main/java/com/company/airbyte/dto/source/file/SourceFileDTO.java
@@ -7,6 +7,8 @@ import com.airbyte.api.models.shared.StorageProvider;
 import com.company.airbyte.dto.source.SourceDTO;
 import io.jmix.core.metamodel.annotation.JmixEntity;
 
+import java.util.Objects;
+
 @JmixEntity
 public class SourceFileDTO extends SourceDTO {
 
@@ -58,5 +60,27 @@ public class SourceFileDTO extends SourceDTO {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        SourceFileDTO that = (SourceFileDTO) o;
+        return Objects.equals(datasetName, that.datasetName)
+                && Objects.equals(format, that.format)
+                && Objects.equals(provider, that.provider)
+                && Objects.equals(readerOptions, that.readerOptions)
+                && Objects.equals(url, that.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                super.hashCode(),
+                datasetName, format, provider, readerOptions, url
+        );
     }
 }
