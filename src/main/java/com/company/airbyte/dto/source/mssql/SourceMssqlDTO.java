eec11faf277288a8f7a7487e2b9fe334bab028c9
NhanhAnhXtanh <nhatanh08112004@gmail.com>
2025-09-03 16:42:04 +0700

    file

--- src/main/java/com/company/airbyte/dto/source/mssql/SourceMssqlDTO.java
+++ src/main/java/com/company/airbyte/dto/source/mssql/SourceMssqlDTO.java
@@ -1,6 +1,5 @@
 package com.company.airbyte.dto.source.mssql;
 
-import com.company.airbyte.dto.source.SourceDTO;
 import com.company.airbyte.dto.source.SourceDatabaseDTO;
 import io.jmix.core.metamodel.annotation.JmixEntity;
 
@@ -11,19 +10,19 @@ public class SourceMssqlDTO extends SourceDatabaseDTO {
 
     private String replicationMethod;
 
-    public SourceMssqlSSLMethod getSslMethod() {
-        return sslMethod == null ? null : SourceMssqlSSLMethod.fromId(sslMethod);
+    public SourceMssqlSSLMethodType getSslMethod() {
+        return sslMethod == null ? null : SourceMssqlSSLMethodType.fromId(sslMethod);
     }
 
-    public void setSslMethod(SourceMssqlSSLMethod sslMethod) {
+    public void setSslMethod(SourceMssqlSSLMethodType sslMethod) {
         this.sslMethod = sslMethod == null ? null : sslMethod.getId();
     }
 
-    public SourceMssqlUpdateMethod getReplicationMethod() {
-        return replicationMethod == null ? null : SourceMssqlUpdateMethod.fromId(replicationMethod);
+    public SourceMssqlUpdateMethodType getReplicationMethod() {
+        return replicationMethod == null ? null : SourceMssqlUpdateMethodType.fromId(replicationMethod);
     }
 
-    public void setReplicationMethod(SourceMssqlUpdateMethod replicationMethod) {
+    public void setReplicationMethod(SourceMssqlUpdateMethodType replicationMethod) {
         this.replicationMethod = replicationMethod == null ? null : replicationMethod.getId();
     }
 
