2fc95f0757d5e1996f6a695e97a8067bcf95509a
kl3inIT <dathip04@gmail.com>
2025-09-05 11:31:11 +0700

    view

--- src/main/java/com/company/airbyte/view/destination/DestinationListView.java
+++ src/main/java/com/company/airbyte/view/destination/DestinationListView.java
@@ -1,9 +1,14 @@
 package com.company.airbyte.view.destination;
 
 import com.company.airbyte.entity.Destination;
+import com.company.airbyte.entity.DestinationType;
 import com.company.airbyte.view.main.MainView;
+import com.vaadin.flow.router.QueryParameters;
 import com.vaadin.flow.router.Route;
+import io.jmix.flowui.ViewNavigators;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Route(value = "destinations", layout = MainView.class)
@@ -12,4 +17,30 @@ import io.jmix.flowui.view.*;
 @LookupComponent("destinationsDataGrid")
 @DialogMode(width = "64em")
 public class DestinationListView extends StandardListView<Destination> {
+
+    @Autowired
+    private ViewNavigators viewNavigators;
+
+    @Subscribe("destinationsDataGrid.createDb")
+    public void onDestinationsDataGridCreateDb(final ActionPerformedEvent event) {
+        String destinationType = DestinationType.DATABASE.getId();
+        viewNavigators.detailView(this, Destination.class)
+                .withQueryParameters(QueryParameters.of("type", destinationType))
+                .newEntity()
+                .navigate();
+    }
+
+    @Subscribe("destinationsDataGrid.createFile")
+    public void onDestinationsDataGridCreateFile(final ActionPerformedEvent event) {
+        String destinationType = DestinationType.FILE.getId();
+        viewNavigators.detailView(this, Destination.class)
+                .withQueryParameters(QueryParameters.of("type", destinationType))
+                .newEntity()
+                .navigate();
+    }
+
+    @Subscribe("destinationsDataGrid.createApi")
+    public void onDestinationsDataGridCreateApi(final ActionPerformedEvent event) {
+        // TODO: Implement API destination creation
+    }
 }
--- src/main/java/com/company/airbyte/view/destination/fragment/DestinationS3Fragment.java
+++ src/main/java/com/company/airbyte/view/destination/fragment/DestinationS3Fragment.java
@@ -1,11 +1,73 @@
 package com.company.airbyte.view.destination.fragment;
 
 import com.company.airbyte.dto.destination.s3.DestinationS3DTO;
+import com.company.airbyte.dto.destination.s3.DestinationS3OutputFormatType;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import io.jmix.core.Metadata;
 import io.jmix.flowui.fragment.Fragment;
 import io.jmix.flowui.fragment.FragmentDescriptor;
 import io.jmix.flowui.fragmentrenderer.FragmentRenderer;
+import io.jmix.flowui.fragmentrenderer.RendererItemContainer;
+import io.jmix.flowui.model.DataContext;
+import io.jmix.flowui.model.InstanceContainer;
+import io.jmix.flowui.view.Subscribe;
+import io.jmix.flowui.view.ViewComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @FragmentDescriptor("destination-s3-fragment.xml")
+@RendererItemContainer("destinationS3Dc")
 public class DestinationS3Fragment extends FragmentRenderer<VerticalLayout, DestinationS3DTO> {
+
+//    @Autowired
+//    private Metadata metadata;
+//
+//    @ViewComponent
+//    private InstanceContainer<DestinationS3DTO> destinationS3Dc;
+//
+//    @ViewComponent
+//    private FormLayout s3CsvForm;
+//
+//    @ViewComponent
+//    private FormLayout s3JsonLinesForm;
+//
+//    private DestinationS3DTO item;
+//
+//    public void setItem(DestinationS3DTO item) {
+//        this.item = item;
+//        destinationS3Dc.setItem(item);
+//        updateFormatVisibility();
+//    }
+//
+//    public DestinationS3DTO getItem() {
+//        return destinationS3Dc.getItem();
+//    }
+//
+//    @Subscribe("destinationS3Dc")
+//    public void onDestinationS3DcItemChange(InstanceContainer.ItemChangeEvent<DestinationS3DTO> event) {
+//        updateFormatVisibility();
+//    }
+//
+//    private void updateFormatVisibility() {
+//        DestinationS3DTO dto = destinationS3Dc.getItem();
+//        if (dto != null && dto.getFormat() != null) {
+//            switch (dto.getFormat()) {
+//                case CSV:
+//                    s3CsvForm.setVisible(true);
+//                    s3JsonLinesForm.setVisible(false);
+//                    break;
+//                case JSON:
+//                    s3CsvForm.setVisible(false);
+//                    s3JsonLinesForm.setVisible(true);
+//                    break;
+//                default:
+//                    s3CsvForm.setVisible(false);
+//                    s3JsonLinesForm.setVisible(false);
+//                    break;
+//            }
+//        } else {
+//            s3CsvForm.setVisible(false);
+//            s3JsonLinesForm.setVisible(false);
+//        }
+//    }
 }
